c326a664a8be43ada64ae4d20dc56546e8d76782
Alexey Levchenko <alev_x@ukr.net>
2026-08-05 10:15:53 +0300

    Оживляем отправку формы

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //находим на странице кнопку и блок, который нужно показать/скрыть
 const button = document.querySelector('#call_back');
-const content = document.querySelector('#content');
+const content = document.querySelector('#callback-form');
 
 //добавляем обработчик клика на кнопку
 button.addEventListener('click', function() {
@@ -14,7 +14,7 @@ button.addEventListener('click', function() {
   }
 });
 
-
+// Связываем базу данных с сайтом (DOM-манипуляции)
 const myProjects = [
   { title: "Адаптивное портфолио", image: "./img/1.jpg", image_alt: "Проект 1", desc: "Мой первый сайт на HTML и CSS с темной темой.", link: "#"},
   { title: "JS Трекер кредитов", image: "./img/2.jpg", image_alt: "Проект 2", desc: "Автоматизированная система проверки баланса на чистом JavaScript.", link: "#"},
@@ -37,3 +37,20 @@ for (const project of myProjects) {
   // 4. Добавляем эту карточку внутрь контейнера
   cardsContainer.insertAdjacentHTML('beforeend', cardHTML);
 }
+
+// Оживляем отправку формы
+// 1. Находим саму форму по её ID
+const contactForm = document.querySelector('#callback-form');
+// 2. Слушаем событие 'submit' (отправка), а не просто клик по кнопке
+contactForm.addEventListener('submit', function(event) {
+  // 3. ПЕРВЫМ ДЕЛОМ отменяем перезагрузку страницы!
+  event.preventDefault();
+
+  // 4. Достаем значения, которые пользователь ввёл в поля (через .value)
+  const userName = document.querySelector('#name').value;
+  const userEmail = document.querySelector('#email').value;
+  // 5. Выводим данные в консоль, чтобы убедиться, что мы их перехватили
+  console.log(`Данные формы перехвачены: Имя - ${userName}, email - ${userEmail}`);
+  // 6. Очищаем поля формы после отправки
+  contactForm.reset();
+});
