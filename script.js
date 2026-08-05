55d4b7c3769e241b137f4df0eb952a9a9a4f41e9
Alexey Levchenko <alev_x@ukr.net>
2026-08-05 09:52:07 +0300

    Практическое задание «База данных пользователей»

--- script.js
+++ script.js
@@ -13,3 +13,26 @@ button.addEventListener('click', function() {
     button.textContent = 'Отмена';
   }
 });
+
+
+const myProjects = [
+  { title: "Адаптивное портфолио", desc: "Мой первый сайт на HTML и CSS с темной темой.", link: "#"},
+  { title: "JS Трекер кредитов", desc: "Автоматизированная система проверки баланса на чистом JavaScript.", link: "#"},
+  { title: "Будущий проект на React", desc: "Здесь будет крутое SPA приложение.", link: "#"}
+]
+
+// 1. Находим пустой контейнер на странице
+const cardsContainer = document.querySelector('.project-cards');
+// 2. Запускаем цикл по нашему массиву проектов
+for (const project of myProjects) {
+  // 3. Создаем HTML-структуру для одной карточки, подставляя данные из объекта
+  const cardHTML = `
+    <div class="project-card">
+      <h3>${project.title}</h3>
+      <p>${project.desc}</p>
+      <a href="${project.link}">Смотреть код</a>
+    </div>
+  `;
+  // 4. Добавляем эту карточку внутрь контейнера
+  cardsContainer.insertAdjacentHTML('beforeend', cardHTML);
+}
